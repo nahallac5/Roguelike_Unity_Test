68d3daf109bcfdfd8d7e8b9e546a8d9e6aad9daf
Liam Callahan <53151930+nahallac5@users.noreply.github.com>
2023-02-23 21:55:07 -0500

    First Real Commit

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    [SerializeField] private bool isSentient = false;
+
+    public bool IsSentient { get => isSentient; }
+
+    void Start() {
+        if (GetComponent<Player>())
+            GameManager.instance.InsertEntity(this, 0);
+        else if (IsSentient)
+            GameManager.instance.AddEntity(this);
+    }
+    
+    public void Move(Vector2 direction) {
+        transform.position += (Vector3)direction;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+
+    [SerializeField] private float time = 0.1f;
+    [SerializeField] private bool isPlayerTurn = true;
+
+    [SerializeField] private int entityNum = 0;
+    [SerializeField] private List<Entity> entities = new List<Entity>();
+    
+    // Guesser
+    public bool IsPlayerTurn {get => isPlayerTurn; }
+
+    // Start is called before the first frame update
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+    }
+    private void StartTurn() {
+        //Debug.Log($"{entities[entityNum].name} starts its turn!");
+        if (entities[entityNum].GetComponent<Player>())
+            isPlayerTurn = true;
+        else if (entities[entityNum].IsSentient)
+            Action.SkipAction(entities[entityNum]); //FIXME -  No logic yet
+    }
+
+    public void EndTurn() {
+        //Debug.Log($"{entities[entityNum].name} ends its turn!");
+        if (entities[entityNum].GetComponent<Player>())
+            isPlayerTurn = false;
+        
+        if (entityNum == entities.Count - 1)
+            entityNum = 0;
+        else
+            entityNum++;
+
+        StartCoroutine(TurnDelay());
+    }
+
+    private IEnumerator TurnDelay() {
+        yield return new WaitForSeconds(time);
+        StartTurn();
+    }
+
+    public void AddEntity(Entity entity) {
+        entities.Add(entity);
+    }
+
+    public void InsertEntity(Entity entity, int index) {
+        entities.Insert(index, entity);
+    }
+}
